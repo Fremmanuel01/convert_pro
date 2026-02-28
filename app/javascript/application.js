d86b9129655b645270294687a2b1e3394766148f
Emmanuel Nwabufo <fremmanuel@shanahanef.org>
2026-02-28 20:34:33 +0100

    Manually install and register Stimulus framework for mobile Javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { setupFirebaseAuth } from "firebase_auth"
+import "controllers"
 
 // Initialize Firebase Auth if the config is present securely
 const firebaseConfigMeta = document.querySelector('meta[name="firebase-config"]')
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "firebase_auth", to: "firebase_auth.js"
 pin "firebase/app", to: "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js"
 pin "firebase/auth", to: "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js"
